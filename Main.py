2a5251ca9473d8acd5c6fb5ee494a2dc0d1903c3
jimmybob321 <jamesmo321@yahoo.ca>
2017-01-24 15:16:06 -0500

    implemented function to check cards in hand

--- Main.py
+++ Main.py
@@ -1,16 +1,17 @@
 import time,pyautogui
 width, height = pyautogui.size()
 
-def playCard(cardx):
-    #850 is center of hand
-    pyautogui.moveTo(850, 925, duration=0.25)
+def playCard(cardx,cardy):
+    #850,925 is center of hand
+    pyautogui.moveTo(cardx, cardy, duration=0.25)
     pyautogui.dragRel(0,-450,duration=1.5)
 
-def passTurn():
+def passTurn(turn):
     #passes turn
     pyautogui.moveTo(2725,975,duration=0.25)
     pyautogui.click()
-
+    turn = False
+    return turn
 def goFace():
     pass
 
@@ -24,13 +25,30 @@ def startGame():
 def think():
     time.sleep(2)
 
+def checkTurn(turn,state):
+    if state.getpixel(positionx, positiony) == (111, 111, 111) or state.getpixel(positionx, positiony) == (111, 111, 111):
+        turn = True
+    return turn
+
 def detectTarget():
     pass
 
-def checkCards(img):
-    #return cardx
+def checkCards(img,numCards):
+    #return number of cards in hand and the position of the card to play
+    positions  =["850-925","850-925 850-925","850-925 850-925","850-925 850-925","850-925 850-925","850-925 850-925","850-925 850-925","850-925 850-925","850-925 850-925","850-925 850-925","850-925 850-925"]
+    positions2 = positions[x].split(" ")
+    count = 0
+    for x in range(0,numCards):
+        a = positions2.split("-")
+        positionX = a[0]
+        positionY = a[1]
+        if state.getpixel(positionx, positiony) ==(111,111,111):
+            if count == 0:
+                cardx = positionx
+                cardy = positiony
+            count +=1
 
-    pass
+    return count,cardx,cardy
 
 def checkMinions():
     pass
@@ -42,15 +60,25 @@ def emote():
     pyautogui.moveTo(725, 800, duration=0.25)
     pyautogui.click()
 
+cards = 3
+turn = False
 while True:
     time.sleep(2)
     state = pyautogui.screenshot()
-    cardx = checkCards(state)
-    while cardx!= 0:
-        playCard(cardx)
-        cardx = checkCards(state)
-    passTurn()
+    turn = checkturn(turn,state)
+    think()
+    state = pyautogui.screenshot()
+    if turn:
+        count,cardx,cardy = checkCards(state,cards)
+        while count!= 0:
+            playCard(cardx,cardy)
+            cards -=1
+
+            count,cardx,cardy = checkCards(state)
+            think()
+
+        turn = passTurn(turn)
 
     break
 
-playCard(850)
+
